e7df4b6ebe5c195d26b1f3dec4631d7219a51283
Gordon <ghprince@gmail.com>
2012-04-01 22:09:55 +0800

    Sina Weibo share button

--- scripts/format.js
+++ scripts/format.js
@@ -1,5 +1,5 @@
 var engLibrary = {
-	txt_title:"Welcome to Draw Something helper!",
+	txt_title:"Welcome to Draw Sth. helper!",
 	txt_instruction:"Input the candidate letters and length of the answer, we'll find the words for you.",
 	txt_letter:"Given Letters (max 12)",
 	txt_length:"Word Length",
@@ -7,7 +7,7 @@ var engLibrary = {
 	txt_counter:"Total Guess Count:"
 };
 var chsLibrary = {
-	txt_title:"欢迎来到 Draw Someting 小助手！",
+	txt_title:"欢迎来到 Draw Sth. 小助手！",
 	txt_instruction:"请输入候选字母和答案长度，让我来帮你猜猜看吧。",
 	txt_letter:"候选字母（最多12个）",
 	txt_length:"答案长度",
